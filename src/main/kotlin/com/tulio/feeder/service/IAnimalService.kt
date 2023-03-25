ddddda93d1b5993b63b31e9d5a4b85a72a1d4a45
Thúlyo Araujo <thulyoaraujo14@gmail.com>
2023-03-24 23:16:23 -0300

    refactor: :hammer: Adiciona DTO como retorno da função findAll
    FS-2
    
    Signed-off-by: Thúlyo Araujo <thulyoaraujo14@gmail.com>

--- src/main/kotlin/com/tulio/feeder/service/IAnimalService.kt
+++ src/main/kotlin/com/tulio/feeder/service/IAnimalService.kt
@@ -1,6 +1,7 @@
 package com.tulio.feeder.service
 
 import com.tulio.feeder.model.dto.AnimalDTO
+import com.tulio.feeder.model.dto.IAnimalDTO
 import com.tulio.feeder.model.entity.Animal
 import com.tulio.feeder.model.form.AnimalForm
 
@@ -11,7 +12,7 @@ interface IAnimalService {
 
     fun createAnimal(animalForm: AnimalForm): AnimalDTO
 
-    fun findAll(): Any
+    fun findAll(): List<IAnimalDTO>
 
     fun updateAnimal(id: Long, animalForm: AnimalForm): Animal
 
