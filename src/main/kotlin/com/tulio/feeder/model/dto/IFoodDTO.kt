4dc03760c621b424157d589b9e87626ebae4e86d
Thúlyo Araujo <thulyoaraujo14@gmail.com>
2023-11-17 21:06:33 -0300

    bugfix: :bug: Cria DTO para não ter erros ao buscar dados dos animais
    FS-6
    
    Signed-off-by: Thúlyo Araujo <thulyoaraujo14@gmail.com>

---
+++ src/main/kotlin/com/tulio/feeder/model/dto/IFoodDTO.kt
@@ -0,0 +1,9 @@
+package com.tulio.feeder.model.dto
+
+/**
+ * Interface usada como DTO para retornar dados específicos das comidas.
+ */
+interface IFoodDTO {
+    var id: Long?
+    var name: String?
+}
